66486a96af51f3083f02bb69504e93da4d070933
Adepoju Lateef <badepojulateef@gmail.com>
2023-01-23 14:43:19 +0100

    attempted task 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,44 @@
+#include "sort.h"
+
+void swap(int *a, int *b);
+
+/**
+ * bubble_sort - sort a given array
+ *
+ * @array: The array(unsorted) to be sorted
+ * @size: The size of the array
+ * Return: Nothing.
+ */
+void bubble_sort(int *array, size_t size)
+{
+	unsigned int i;
+	unsigned int j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(array + j, array + j + 1);
+				print_array(array, size);
+			}
+		}
+	}	
+}
+
+/**
+ * swap - swaps two members of an array
+ *
+ * @a: First parameter
+ * @b: Second parameter
+ * Return: Nothing.
+ */
+void swap(int *a, int *b)
+{
+	unsigned int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,12 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sorts a linked list
+ *
+ * @list: the linked list to be sorted
+ * Return: Nothing.
+ */
+void insertion_sort_list(listint_t **list)
+{
+	
+}
